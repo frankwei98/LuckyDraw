1a6f6cde8322c2d70da7be1ba474cfc9a4cfc022
Frank Wei <frank@frankwei.xyz>
2018-05-08 12:08:29 +0800

    feat: router and ABI

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Draw from '@/views/Draw'
 import Home from '@/views/Home'
-
+import LuckyMoney from '@/views/LuckyMoney'
+import MyView from '@/views/My'
 Vue.use(Router)
 
 export default new Router({
@@ -20,6 +21,16 @@ export default new Router({
       path: '/draw',
       name: 'Draw',
       component: Draw
+    },
+    {
+      path: '/luckyMoney/:id',
+      name: 'LuckyMoney',
+      component: LuckyMoney
+    },
+    {
+      path: '/my',
+      name: 'MyView',
+      component: MyView
     }
   ]
 })
