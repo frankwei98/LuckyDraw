e7f3f0328ecd47fab24b9a0248e74a9b027b479d
Frank Wei <frank@frankwei.xyz>
2018-05-08 12:07:11 +0800

    fix: change to experimental contract

--- src/web3/config.js
+++ src/web3/config.js
@@ -23,13 +23,18 @@ export const network = {
   42: {
     // default: true,
     name: 'Kovan Test Network',
-    contract: '0xb760ada4f12e5d29eff2d9eef3bca94b574a1f2f',
+    // contract: '0xb760ada4f12e5d29eff2d9eef3bca94b574a1f2f',
+    contract: '0x7034B866Bcb60eF7ec71AdFe88259B3C93b473Ee',
+    ERC20Package: '0xa35d790f98ab834a93f129c38011ebebe1cdfea7',
+    ERC20: '0xC608A405D5079F407C1ab98446aeCd4deE6496B1',
     rpc: 'https://kovan.infura.io/2CEbuqs01htfmb4oL5EB'
   },
   5: {
     // default: true,
     name: 'Private Network',
-    contract: '0x58a84ee22f8e6f371db58393b9d65420cf068573',
+    contract: '0xbfc0d79e791dc9cd8b242a008ed0510e42a2de',
+    ERC20Package: '0x299001887ed07bdc619688176231a865264ae',
+    ERC20: '0x8fed7212efd7ed42c38242105875bffaa2fff',
     rpc: 'http://127.0.0.1:7546'
   }
 }
